0da728c6710af1cf412d65a35de6e5f45f1960f9
fr-neil <nileshaher141@gmail.com>
2024-11-12 17:25:06 +0530

    added calender view

--- src/routes/calendar.tsx
+++ src/routes/calendar.tsx
@@ -1,7 +1,44 @@
+import { Calendar as BigCalendar } from 'react-big-calendar';
+import { dateFnsLocalizer } from 'react-big-calendar';
+import { format } from 'date-fns/format';
+import { parse } from 'date-fns/parse';
+import { startOfWeek } from 'date-fns/startOfWeek';
+import { getDay } from 'date-fns/getDay';
+import { enUS } from 'date-fns/locale/en-US';
+import 'react-big-calendar/lib/css/react-big-calendar.css';
+
+const locales = {
+    'en-US': enUS,
+};
+
+const localizer = dateFnsLocalizer({
+    format,
+    parse,
+    startOfWeek,
+    getDay,
+    locales,
+});
+
 export default function CalendarPage() {
+    const events = [
+        {
+            title: 'Test Appointment',
+            start: new Date(),
+            end: new Date(new Date().setHours(new Date().getHours() + 1)),
+        },
+    ];
+
     return (
-        <div className='h-screen w-screen flex items-center justify-center'>
-            <h1 className='text-2xl font-bold'>Calendar</h1>
+        <div className='h-screen p-4'>
+            <BigCalendar
+                localizer={localizer}
+                events={events}
+                startAccessor='start'
+                endAccessor='end'
+                style={{ height: 'calc(100vh - 2rem)' }}
+                defaultView='week'
+                views={['week', 'day']}
+            />
         </div>
     );
 }
